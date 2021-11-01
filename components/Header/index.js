3afc52df94cdfe43b6c522a0f4c7a54c2483d152
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-11-01 17:58:07 -0600

    RFC validation

--- components/Header/index.js
+++ components/Header/index.js
@@ -51,13 +51,9 @@ const Header = () => {
                   </div>
                 </div>
                 <div className="px-2 pt-2 pb-3 space-y-1">
-                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
-
-                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-
-                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-
-                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
+                  <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Nuestra API</a>
+                  <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Legales</a>
+                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Entrar</a>
                 </div>
                 <a href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
                   Log in
