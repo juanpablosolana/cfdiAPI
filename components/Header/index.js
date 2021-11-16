f6ebe22e3dedf5e57644a56bb05db036217282d8
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-11-16 11:31:59 -0600

    responsive ok

--- components/Header/index.js
+++ components/Header/index.js
@@ -13,11 +13,11 @@ const Header = () => {
  const [example, setExample] = useState('')
 
   return (
-    <div className="relative bg-white overflow-hidden max-h-screen">
+    <div className="relative bg-white overflow-hidden h-screen">
       <HelpInfo state={isOpen} setState={setIsOpen} setRfcEmisor={setRfcEmisor} setRfcReceprot={setRfcReceptor} setTotal={setTotal} setFolioFiscal={setFolioFiscal} setExample={setExample} />
       <div className="max-w-7xl mx-auto ">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+          <svg className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
