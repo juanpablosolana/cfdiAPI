14e0a75ab191e0666ed39a7c3019889155e73fc9
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-06-23 19:37:25 -0500

    Refactor components-Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,63 +1,69 @@
 import Image from 'next/image'
-// import Link from 'next/link'
+import Menu from '../Menu'
 import Form from '../Hero/Form'
 import HelpInfo from '../HelpInfo'
 import LeftHero from '../Hero/LeftHero'
 import { useState } from 'react'
 
 const Header = () => {
- const [isOpen, setIsOpen] = useState(false)
- const [rfcEmisor, setRfcEmisor] = useState('')
- const [rfcReceptor, setRfcReceptor] = useState('')
- const [total, setTotal] = useState('')
- const [folioFiscal, setFolioFiscal] = useState('')
- const [example, setExample] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [rfcEmisor, setRfcEmisor] = useState('')
+  const [rfcReceptor, setRfcReceptor] = useState('')
+  const [total, setTotal] = useState('')
+  const [folioFiscal, setFolioFiscal] = useState('')
+  const [example, setExample] = useState('')
 
   return (
     <div className="relative bg-white overflow-hidden">
-      <HelpInfo state={isOpen} setState={setIsOpen} setRfcEmisor={setRfcEmisor} setRfcReceprot={setRfcReceptor} setTotal={setTotal} setFolioFiscal={setFolioFiscal} setExample={setExample} />
+      <HelpInfo
+        state={isOpen}
+        setState={setIsOpen}
+        setRfcEmisor={setRfcEmisor}
+        setRfcReceprot={setRfcReceptor}
+        setTotal={setTotal}
+        setFolioFiscal={setFolioFiscal}
+        setExample={setExample}
+      />
       <div className="max-w-7xl mx-auto ">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
           <div>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                      <span className="sr-only">Workflow</span>
-                      <Image className="h-8 w-auto" src="/img/workflow-mark-indigo-600.svg" width={50} height={50} alt="Logo" />
-                    </a>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  {/* <Link href="nuestra-api" className="font-medium text-gray-500 hover:text-gray-900"><a>Nuestra API</a></Link> */}
-                  <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Legales</a>
-                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Entrar</a>
-                </div>
-              </nav>
+              <Menu />
             </div>
 
             <div className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
               <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                 <div className="px-5 pt-4 flex items-center justify-between">
                   <div>
-                    <Image className="h-8 w-auto" src="/img/workflow-mark-indigo-600.svg" width={50} height={50} alt="Logo" />
+                    <Image
+                      className="h-8 w-auto"
+                      src="/img/workflow-mark-indigo-600.svg"
+                      width={50}
+                      height={50}
+                      alt="Logo"
+                    />
                   </div>
                   <div className="-mr-2">
                     <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span className="sr-only">Close main menu</span>
-                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
                       </svg>
                     </button>
                   </div>
@@ -71,11 +77,18 @@ const Header = () => {
             </div>
           </div>
 
-          <LeftHero setIsOpen={setIsOpen}/>
+          <LeftHero setIsOpen={setIsOpen} />
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 bg-indigo-300">
-        <Form rfcEmisor={rfcEmisor} rfcReceptor={rfcReceptor} total={total} folioFiscal={folioFiscal} example={example} setExample={setExample}/>
+        <Form
+          rfcEmisor={rfcEmisor}
+          rfcReceptor={rfcReceptor}
+          total={total}
+          folioFiscal={folioFiscal}
+          example={example}
+          setExample={setExample}
+        />
       </div>
     </div>
   )
--- pages/nuestra-api.js
+++ pages/nuestra-api.js
@@ -1,7 +1,99 @@
-const API = ()=>{
-  return(
-    <h1>Nuestra API</h1>
+/* This example requires Tailwind CSS v2.0+ */
+const features = [
+  { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+  { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+  { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+  { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+  { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+  { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+]
+import Menu from '../components/Menu'
+import Image from 'next/image'
+
+export default function Example() {
+  return (
+    <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto ">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg className="hidden lg:block absolute right-0 inset-y-0 h-screen w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          <div>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <Menu/>
+            </div>
+
+            <div className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="px-5 pt-4 flex items-center justify-between">
+                  <div>
+                    <Image className="h-8 w-auto" src="/img/workflow-mark-indigo-600.svg" width={50} height={50} alt="Logo" />
+                  </div>
+                  <div className="-mr-2">
+                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Close main menu</span>
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                  <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Nuestra API</a>
+                  <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Legales</a>
+                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Entrar</a>
+                </div>
+              </div>
+            </div>
+
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 bg-indigo-300">
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+              className="bg-gray-100 rounded-lg"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
+              alt="Top down view of walnut card tray with embedded magnets and card groove."
+              className="bg-gray-100 rounded-lg"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
+              alt="Side of walnut card tray with card groove and recessed card area."
+              className="bg-gray-100 rounded-lg"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
+              alt="Walnut card tray filled with cards and card angled in dedicated groove."
+              className="bg-gray-100 rounded-lg"
+            />
+          </div>      </div>
+    </div>
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+        <div>
+
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+          <p className="mt-4 text-gray-500">
+            The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
+            steel divider separates active cards from new ones, or can be used to archive important task lists.
+          </p>
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {features.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{feature.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+
+      </div>
+    </div>
+    </div>
   )
 }
-
-export default API;
