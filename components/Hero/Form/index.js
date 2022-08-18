6e8b5d93ecba7708a4ef47f084f6b9b3e866648f
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-08-17 19:15:27 -0500

    Tailwind refactor

--- components/Hero/Form/index.js
+++ components/Hero/Form/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Spinner from '../../Spinner'
+import { customCss } from "../../../const";
 
 const Form = ({ rfcEmisor, rfcReceptor, total, folioFiscal, example, setExample }) => {
   const { register, handleSubmit, reset, setValue } = useForm({ mode: 'onBlur' });
@@ -34,7 +35,7 @@ const Form = ({ rfcEmisor, rfcReceptor, total, folioFiscal, example, setExample 
         setLoading(false)
         setTimeout(() => {
           setStatus(null)
-        }, 5000);
+        }, 10000);
       })
       .catch(err => {
         setStatus(err)
@@ -44,45 +45,49 @@ const Form = ({ rfcEmisor, rfcReceptor, total, folioFiscal, example, setExample 
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
       <div className="max-w-md w-full space-y-8 ">
-        <label className="flex justify-center text-gray-800 ml-1">Ingresa los datos de la factura</label>
+        <label className="flex justify-center font-bold text-gray-800 ml-1 text-xl">Ingresa los datos de la factura</label>
         <form id="api-form" className="mt-8 space-y-6 " onSubmit={handleSubmit(onSubmit)}>
           <input
             placeholder="RFC Emisor"
             id="rfcEmisor"
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            className={customCss.form}
             {...register("rfcEmisor", { required: true, minLength: 12, maxLength: 15 })} />
           <input
             placeholder="RFC Receptor"
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            className={customCss.form}
             {...register("rfcReceptor", { required: true, minLength: 12, maxLength: 15 })} />
           <input
             placeholder="Total con centavos"
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            className={customCss.form}
             type="number"
             step="0.01"
             {...register("total", { required: true, min: 0 })} />
           <input
-            placeholder="Folio fiscal / folioFiscal tal como aparece en la factura"
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Folio fiscal / UUID"
+            className={customCss.form}
             {...register("folioFiscal", { required: true, minLength: 36, maxLength: 36 })} />
           {loading
-          ? <Spinner />
-          : <input
-            id="submit"
-            type="submit"
-            className={example
-              ? "animate-bounce group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              : "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"} />}
+            ? <Spinner />
+            : <input
+              id="submit"
+              type="submit"
+              value="Enviar"
+              className={
+                example
+                  ? `${customCss.animation} ${customCss.button}`
+                  : customCss.button
+              } />
+          }
         </form>
         {
           status && <div
             className={status === 'Vigente'
-            ? "bg-green-100 border-l-4 border-green-500 text-green-700 p-4"
-            : "bg-red-100 border-l-4 border-red-500 text-red-700 p-4"}>
-              <h1>Estatus: {status}</h1>
-              <h2>Código: {statusCode} </h2>
-              <h2>Es cancelable: {cancelCode}</h2>
-            </div>
+              ? "bg-green-100 border-l-4 border-green-500 text-green-700 p-4"
+              : "bg-red-100 border-l-4 border-red-500 text-red-700 p-4"}>
+            <h1> Estatus: {status} </h1>
+            <h2> Código: {statusCode} </h2>
+            <h2> Es cancelable: {cancelCode} </h2>
+          </div>
         }
       </div>
     </div>
--- const/index.js
+++ const/index.js
@@ -1,9 +1,11 @@
-const url = 'https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc';
+const url = 'https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc'
+
 const sampleHeaders = {
   'user-agent': 'sampleTest',
   'Content-Type': 'text/xml;charset=UTF-8',
   'soapAction': 'http://tempuri.org/IConsultaCFDIService/Consulta',
-};
+}
+
 const xml = (values) => {
   return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
      <soapenv:Header/>
@@ -17,8 +19,15 @@ const xml = (values) => {
   </soapenv:Envelope>`;
 }
 
+const customCss = {
+  form: 'appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm',
+  animation: 'animate-bounce',
+  button: 'group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+}
+
 export {
   url,
   sampleHeaders,
-  xml
+  xml,
+  customCss
 }
