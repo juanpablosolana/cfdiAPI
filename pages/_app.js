f728a9a31acb7317f3a0f313611de6fcd3df270e
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2024-04-25 11:49:57 -0600

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
-import * as ga from "../lib/ga";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
 
 export default MyApp;
